2a9a5f4de713b7152987ac3ab163eadb6d03cef1
Varun Tiwari <tiwarivarun819.vt33.vt@gmail.com>
2022-07-31 11:13:24 +0530

    :bug: Export bug in ErrorMiddleware fixed

--- src/ErrorMiddleware.ts
+++ src/ErrorMiddleware.ts
@@ -1,12 +1,12 @@
-import { NextFunction, Request, Response, ErrorRequestHandler } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { ErrorHandler } from './ErrorHandler';
 
-module.exports = (
+export function ErrorMiddleware(
   err: ErrorHandler,
   req: Request,
   res: Response,
   next: NextFunction
-) => {
+) {
   err.statusCode = err.statusCode || 500;
 
   err.message = err.message || 'Internal Server error';
@@ -15,4 +15,4 @@ module.exports = (
     success: false,
     message: err.message,
   });
-};
+}
